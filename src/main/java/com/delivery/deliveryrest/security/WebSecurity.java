08884a60e4ba78c80bf0923417d40fa1f147507a
ccDpE9 <kostadamjanovic8080@gmail.com>
2024-05-21 22:41:38 +0200

    Form-login configuration that disables HTML response on unauthenticated requests, defines custom URL for processing authentication and changes the values user sends on login request from 'username' field in UsernamePasswordAuthenticationFiter to 'email'

--- src/main/java/com/delivery/deliveryrest/security/WebSecurity.java
+++ src/main/java/com/delivery/deliveryrest/security/WebSecurity.java
@@ -2,6 +2,7 @@ package com.delivery.deliveryrest.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -20,8 +22,13 @@ public class WebSecurity {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+				.exceptionHandling(exceptions -> exceptions
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.NOT_IMPLEMENTED)))
+				.formLogin(form -> form
+						.loginProcessingUrl("/authenticate")
+						.usernameParameter("email"))
 				.authorizeHttpRequests((authorize) -> authorize
-						.anyRequest().authenticated());
+						.anyRequest().permitAll());
 
 		return http.build();
 	}
