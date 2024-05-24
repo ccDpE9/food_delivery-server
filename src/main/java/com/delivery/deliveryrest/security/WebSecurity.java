69ca82f8c0de17ecb1c7d933df51813ffb406498
ccDpE9 <kostadamjanovic8080@gmail.com>
2024-05-24 20:32:48 +0200

    Changed status code to 200. Added success and failure handlers that don't redirect to HTML pages, which they do by default. This way, they only respond with status code.

--- src/main/java/com/delivery/deliveryrest/security/WebSecurity.java
+++ src/main/java/com/delivery/deliveryrest/security/WebSecurity.java
@@ -3,17 +3,11 @@ package com.delivery.deliveryrest.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +17,10 @@ public class WebSecurity {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.exceptionHandling(exceptions -> exceptions
-						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.NOT_IMPLEMENTED)))
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.OK)))
 				.formLogin(form -> form
+						.successHandler((req, res, auth) -> res.setStatus(201))
+						.failureHandler(new SimpleUrlAuthenticationFailureHandler())
 						.loginProcessingUrl("/authenticate")
 						.usernameParameter("email"))
 				.authorizeHttpRequests((authorize) -> authorize
