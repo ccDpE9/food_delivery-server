2658f94bd73ace2923ddb9703523b61ca460807a
ccDpE9 <kostadamjanovic8080@gmail.com>
2024-06-04 22:19:07 +0200

    If the request contains a request header, use CsrfTokenRequestAttributeHandler. If the request contains request parameter (_csrf), use XorCsrfTokenRequestAttributeHandler to resolve the token.

---
+++ src/main/java/com/delivery/deliveryrest/security/SpaCsrfTokenRequestHandler.java
@@ -0,0 +1,30 @@
+package com.delivery.deliveryrest.security;
+
+import java.util.function.Supplier;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
+import org.springframework.util.StringUtils;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+final class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler {
+	private final CsrfTokenRequestHandler delegate = new XorCsrfTokenRequestAttributeHandler();
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response, Supplier<CsrfToken> csrfToken) {
+		this.delegate.handle(request, response, csrfToken);
+	}
+
+	@Override
+	public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
+		System.out.println("SPA RESOLVE");
+		if (StringUtils.hasText(request.getHeader(csrfToken.getHeaderName()))) {
+			return super.resolveCsrfTokenValue(request, csrfToken);
+		}
+		return this.delegate.resolveCsrfTokenValue(request, csrfToken);
+	}
+}
